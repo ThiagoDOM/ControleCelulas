24b1b8d7fc782ed7c20de9b143db0b25a9fe8349
Thiago Moreira <thiago@marcasite.com.br>
2024-03-21 22:53:24 -0300

    Tradução Login + PWA

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -21,7 +21,10 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('admin.dashboard')">
-                                    <ApplicationLogo
+                                    <!-- <ApplicationLogo
+                                        class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                                    /> -->
+                                    <img src="/android-chrome-192x192.png"
                                         class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
                                     />
                                 </Link>
