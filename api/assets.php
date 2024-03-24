efd7097032ba29b577e28e48fcf61fdb1e144840
Thiago Moreira <thiago@marcasite.com.br>
2024-03-23 22:17:48 -0300

    deploy manifest

--- api/assets.php
+++ api/assets.php
@@ -7,7 +7,12 @@
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
     echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'js') {
+}
+if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
     echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+}
+if ($_GET['type' === 'webmanifest']){
+    header('Content-Type: application/manifest+json; charset: UTF-8');
+    echo require __DIR__ . '/../public/manifest.webmanifest';
 }
