0a3225199592ded246162cba268e8c43abe940c7
Thiago Moreira <thiago@marcasite.com.br>
2024-03-24 00:57:40 -0300

    install pwa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,30 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Controle de Células') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'Controle de Células') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js'])
-        @inertiaHead
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <meta name="description" content="Controle Células">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/manifest.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js'])
+    @inertiaHead
+
+    <meta name="description" content="Controle Células">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <script>
+        var beforeInstallPrompt = null;
+
+        window.addEventListener("beforeinstallprompt", eventHandler, errorHandler);
+
+        function eventHandler(event) {
+            beforeInstallPrompt = event;
+            document.getElementById("installBtn").removeAttribute("disabled");
+        }
+
+        function errorHandler(event) {
+            console.log("error: " + event);
+        }
+
+        function instalar() {
+            if (beforeInstallPrompt) beforeInstallPrompt.prompt();
+        }
+    </script>
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+
+    <button id="installBtn" onclick="instalar()" disabled > Instalar PWA </button>
+</body>
+
 </html>
