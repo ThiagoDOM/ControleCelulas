4fe5a100aa97ea45b1af7b0088d21317cb734536
Thiago Daniel Oliveira Moreira <thiago@marcasite.com.br>
2024-07-02 09:18:18 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         // JsonResource::withoutWrapping();
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') === 'production' || env('APP_URL') == 'https://celulas.thiagodom.dev.br') {
             // URL::forceSchema('https');
             $this->app['request']->server->set('HTTPS', true);
         }
