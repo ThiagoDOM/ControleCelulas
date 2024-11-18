bedb04452b461069816619002f6b47a359fd77e1
Thiago Daniel Oliveira Moreira <50925270+ThiagoDOM@users.noreply.github.com>
2024-11-18 14:12:11 -0300

    database prefix

--- config/database.php
+++ config/database.php
@@ -72,7 +72,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => '',
+            'prefix' => 'cc_',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
